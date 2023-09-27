72c5d4e46ecee0aeb0d97efae83457f3d93849f0
Anaclaraduartem <anaclaraduartemiranda03@gmail.com>
2023-09-27 15:45:03 -0300

    fizemos uma props

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { StyleSheet, Dimensions,  View } from 'react-native';
+import carrinho from "../../mocks/carrinho";
 import Detalhe from "./components/Detalhe";
 
 
 import Topo from './components/Topo';
 
 const width = Dimensions.get('screen').width;
+
 export default function Carrinho () {
     return <> 
-<Topo/>
+
+<Topo titulo={carrinho.topo.titulo}/>
+
     <View style={estilos.carrinho}>
        <Detalhe/>
     </View>
